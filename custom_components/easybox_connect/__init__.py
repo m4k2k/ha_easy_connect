635d382c41cb593c6d062992057d644dd1da47a9
Markus <m4k.join@outlook.com>
2023-05-04 22:46:25 +0200

    add __init

---
+++ custom_components/easybox_connect/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+
+
+"""
+EasyBox integration using DataUpdateCoordinator.
+
+"""
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.debug("START %s", __file__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up from a config entry, forward to sensor setup"""
+    _LOGGER.debug("ENTER async_setup_entry of %s", __file__)
+    # forward setup to sensor.py
+    await hass.config_entries.async_forward_entry_setups(entry, [Platform.SENSOR])
+    return True
